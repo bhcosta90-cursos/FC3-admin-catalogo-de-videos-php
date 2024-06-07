0f04d036612630b5916bc1a91c01b7cb1c5e7673
Bruno Costa <bhcosta90@gmail.com>
2024-06-07 20:15:16 -0300

    CategoryShowApplication Feature Test

---
+++ tests/Feature/Application/Category/CategoryShowApplicationTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+use App\Models\Category;
+use Core\Application\Category\CategoryShowApplication;
+use Core\Application\Category\DataTransfer\CategoryOutput;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+
+describe('CategoryShowApplication Feature Test', function () {
+    test("create a new entity", function () {
+        $entity = Category::factory()->create();
+
+        $application = new CategoryShowApplication(
+            repository: app(CategoryRepositoryInterface::class)
+        );
+
+        $response = $application->handle($entity->id);
+        expect($response)->toBeInstanceOf(CategoryOutput::class)
+            ->name->toBe($entity->name)
+            ->description->toBe($entity->description)
+            ->id->toBe($entity->id)
+            ->created_at->toBe($entity->created_at->format('Y-m-d H:i:s'));
+    });
+});
