478646af71bb65ca310ef586eff5ad4e54d89179
Bruno Costa <bhcosta90@gmail.com>
2024-06-07 14:34:17 -0300

    formatacao do pint

--- tests/Unit/Application/Category/CategoryPaginateApplicationTest.php
+++ tests/Unit/Application/Category/CategoryPaginateApplicationTest.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 
-use Core\Application\Category\DataTransfer\{CategoriesOutput, CategoryShowInput};
-use Core\Application\Category\{CategoryPaginateApplication, CategoryShowApplication};
+use Core\Application\Category\DataTransfer\{CategoriesOutput};
+use Core\Application\Category\{CategoryPaginateApplication};
 use Core\Domain\Repository\CategoryRepositoryInterface;
 use Tests\Unit\Application\Traits\CategoryRepositoryInterfaceTrait;
 
@@ -15,21 +15,21 @@ describe('Category Paginate Application Unit Test', function () {
             ->once();
 
         $application = new CategoryPaginateApplication(repository: $repository);
-        $response = $application->handle();
+        $response    = $application->handle();
 
         expect($response)->toBeInstanceOf(CategoriesOutput::class)
             ->items->toHaveCount(0);
     });
 
     test('when list is not empty', function () {
-        $register = new stdClass();
-        $register->id = 'id';
-        $register->name = 'name';
+        $register              = new stdClass();
+        $register->id          = 'id';
+        $register->name        = 'name';
         $register->description = 'description';
-        $register->is_active = 'is_active';
-        $register->created_at = 'created_at';
-        $register->updated_at = 'created_at';
-        $register->deleted_at = 'created_at';
+        $register->is_active   = 'is_active';
+        $register->created_at  = 'created_at';
+        $register->updated_at  = 'created_at';
+        $register->deleted_at  = 'created_at';
 
         $mockPagination = mockPagination([
             $register,
@@ -41,7 +41,7 @@ describe('Category Paginate Application Unit Test', function () {
             ->once();
 
         $application = new CategoryPaginateApplication(repository: $repository);
-        $response = $application->handle();
+        $response    = $application->handle();
 
         expect($response)->toBeInstanceOf(CategoriesOutput::class)
             ->items->toHaveCount(1);
