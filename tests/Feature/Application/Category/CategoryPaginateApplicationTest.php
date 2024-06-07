5c3efc98a43500d77974d0d3e1a0b2e1f706b26c
Bruno Costa <bhcosta90@gmail.com>
2024-06-07 20:24:04 -0300

    CategoryPaginateApplication Feature Test

---
+++ tests/Feature/Application/Category/CategoryPaginateApplicationTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use App\Models\Category;
+use Core\Application\Category\CategoryPaginateApplication;
+use Core\Application\Category\DataTransfer\CategoryOutput;
+use Core\Application\Category\DataTransfer\CategoryUpdateInput;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+
+describe('CategoryPaginateApplication Feature Test', function () {
+    test("create a new entity", function () {
+        $entity = Category::factory(20)->create();
+
+        $application = new CategoryPaginateApplication(
+            repository: app(CategoryRepositoryInterface::class)
+        );
+
+        $response = $application->handle();
+
+        expect($response)
+            ->total->toBe(20)
+            ->items->toHaveCount(10)
+            ->last_page->toBe(2)
+            ->first_page->toBe(1)
+            ->current_page->toBe(1)
+            ->per_page->toBe(10)
+            ->to->toBe(1)
+            ->from->toBe(10);
+    });
+});
