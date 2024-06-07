5f144eff20e28312d386ecee5e99bb7d462e0f38
Bruno Costa <bhcosta90@gmail.com>
2024-06-07 20:11:41 -0300

    CategoryDeleteApplication Feature Test

--- tests/Feature/Application/Category/CategoryDeleteApplicationTest.php
+++ tests/Feature/Application/Category/CategoryDeleteApplicationTest.php
@@ -4,8 +4,6 @@ use App\Models\Category;
 use Core\Application\Category\CategoryDeleteApplication;
 use Core\Domain\Repository\CategoryRepositoryInterface;
 
-use function PHPUnit\Framework\assertInstanceOf;
-
 describe('CategoryDeleteApplication Feature Test', function(){
     test("delete a entity", function(){
         $entity = Category::factory()->create();
